5ae9e9ec76761d41f39667bc048a44f08e790a2a
anton skoromniy <you@example.com>
2021-03-17 17:04:30 +0200

    style (SearchForm.module.css) fix sticky search form

--- src/components/mainPage/gifCategories/GifCategories.jsx
+++ src/components/mainPage/gifCategories/GifCategories.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import s from "./GigCategories.module.css";
+import s from "./GifCategories.module.css";
 
 const GifCategories = ({ setCategoriesSearchGif }) => {
   return (
